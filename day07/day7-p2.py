617e7f52c57a7e236a073cc6fb476cbd6cffd155
AugFJTan <fj_tan@hotmail.com>
2024-12-07 17:37:34 +0800

    Add Day 7 solution

---
+++ day07/day7-p2.py
@@ -0,0 +1,21 @@
+def operate(nums, result, test):
+    if nums == []:
+        return result == test
+
+    a = operate(nums[1:], result + nums[0], test)
+    b = operate(nums[1:], result * nums[0], test)
+    c = operate(nums[1:], int(str(result) + str(nums[0])), test)
+
+    return a or b or c
+
+total = 0
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        test, nums = line.split(':')
+        test = int(test)
+        nums = list(map(int, nums.split()))
+        if operate(nums[1:], nums[0], test):
+            total += test
+
+print(total)
